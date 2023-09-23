afeb14748d47c1f092f4c313c50c3ea56d7f53cf
Jorge Paiva <110950713+DevJPaiva@users.noreply.github.com>
2023-09-23 16:01:46 -0300

    calculadora imc v2

---
+++ bin/imc_calculadora_v2_intellij.dart
@@ -0,0 +1,5 @@
+import 'package:imc_calculadora_v2_intellij/imc_calculadora_v2_intellij.dart' as imc_calculadora_v2_intellij;
+
+void main(List<String> arguments) {
+  print('Hello world: ${imc_calculadora_v2_intellij.calculate()}!');
+}
---
+++ test/imc_calculadora_v2_intellij_test.dart
@@ -0,0 +1,8 @@
+import 'package:imc_calculadora_v2_intellij/imc_calculadora_v2_intellij.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
