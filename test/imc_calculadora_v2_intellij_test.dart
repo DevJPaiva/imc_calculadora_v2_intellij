67ce6cad088b666750bd3662542bb8da3e2bb346
Jorge Paiva <110950713+DevJPaiva@users.noreply.github.com>
2023-09-25 12:54:12 -0300

    Update imc_calculadora_v2_intellij_test.dart

--- test/imc_calculadora_v2_intellij_test.dart
+++ test/imc_calculadora_v2_intellij_test.dart
@@ -1,29 +1,20 @@
-import 'package:test/test.dart';
-import 'dart:io';
 
+import 'package:test/test.dart';
+import 'package:imc_calculadora_v2_intellij/classe_pessoa.dart';
 import 'package:imc_calculadora_v2_intellij/funcao_verificacao.dart';
 
 void main() {
-  test('Teste de classificação do IMC (Magreza grave)', () {
-    final expectedOutput = 'Você está em Magreza grave\n';
-    final actualOutput = capturePrint(() {
-      verificaImc(15.9);
-    }).toLowerCase(); // Converte a saída real para minúsculas
+  test('Teste de cálculo do IMC', () {
 
-    expect(actualOutput, contains(expectedOutput.toLowerCase()));
+    var pessoa = Pessoa('João', 1.80, 80.0);
+
+    expect(pessoa.nome, 'João');
+    expect(pessoa.altura, 1.80);
+    expect(pessoa.peso, 80.0);
+
+    var imc = pessoa.peso / (pessoa.altura * pessoa.altura);
+    var imcArredondado = imc.toStringAsFixed(2);
+
+    expect(imcArredondado, '24.69');
   });
-
-  // Adicione mais testes para outras classificações do IMC conforme necessário
-}
-
-String capturePrint(void Function() testFn) {
-  final originalOut = stdout;
-  final buffer = StringBuffer();
-  stdout = IOSink(buffer);
-  try {
-    testFn();
-    return buffer.toString();
-  } finally {
-    stdout = originalOut;
-  }
 }
